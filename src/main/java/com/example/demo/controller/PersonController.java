0d28fd0a96060d62623e1a67d1f134aab9d1318b
Ivan <iv4n.eng.2@gmail.com>
2022-12-06 16:24:42 +0800

    delete person

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -61,4 +61,11 @@ public class PersonController {
         personService.updatePerson(p);
         return "redirect:/persons/list";
     }
+
+    @PostMapping(value = "/deletePerson")
+    public String deletePerson(@ModelAttribute(value = "person") Person p) {
+        personService.removePerson(p);
+        return "redirect:/persons/list";
+    }
+
 }
